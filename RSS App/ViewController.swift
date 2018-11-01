8e94bf1263e4c941fb0efd9add3072963905f404
ranmaru <mikami.ryosuke950409@gmail.com>
2018-11-01 23:47:51 +0900

    Initial Commit

---
+++ RSS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSS App
+//
+//  Created by Mikami Ryosuke on 2018/11/01.
+//  Copyright © 2018年 Mikami Ryosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
